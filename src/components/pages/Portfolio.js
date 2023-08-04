72c7f6a0790bf4782a762b808e50c91755bb8061
iburley33 <iburley33@gmail.com>
2023-08-04 12:36:02 -0400

    BH ss added

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Project from '../Project';
 import velocity from '../../imgs/velocity.png';
-
+import BH from '../../imgs/BH SS.png'
 import eatball from '../../imgs/eatball.png'
 
 export default function Portfolio() {
   const propsArray = [
     {
-      name: "Velocity",
-      desc: "Your one stop shop for all things cycling. Find your nearest cycling route and current weather conditions in 1 quick search.",
+      name: "Burley Home (Current Work in Progress)",
+      desc: "Full service E-Commerce app for home design and furniture shopping",
       link: 'https://young-river-94596.herokuapp.com/login',
-      image: velocity
+      image: BH
     },
     {
       name: "The Magic Eat Ball!",
@@ -29,7 +29,7 @@ export default function Portfolio() {
 
     <div class='portfolio'>
       <div class='portHead'>
-        <h1>Portfolio</h1>
+        <h1>Featured Projects</h1>
       </div>
       {propsArray.map((project) => {
         return <Project name={project.name} description={project.desc} image={project.image} link={project.link} />
